e7a2d2456a8df2aab57454693e312de2e5202c7e
ricky_huangyunqi <huangyunqi2017>
2019-05-13 11:55:40 +0800

    webpack,babel config

---
+++ src/components/PageFrame.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/App.scss";
+import PropTypes from "prop-types";
+export default class PageFrame extends React.Component {
+    componentDidMount () {
+    }
+    render () {
+        return (
+            <div className="x-page-frame">{this.props.children}</div>
+        );
+    }
+}
+PageFrame.propTypes = {
+    children: PropTypes.element
+};
